3af7375adeb93cbdbbc597308ecedb7f8d7fdb21
Lucas <lucasafernandes94@gmail.com>
2016-08-09 23:10:35 -0300

    Update server.js

--- app/server.js
+++ app/server.js
@@ -1,10 +1,6 @@
 var http = require('http');
 var app = require('./config/express');
 
-var uriBanco = 'mongo/teste';
-
-require('./config/database')(uriBanco);
-
 http.createServer(app)
 .listen(3000, function() {
 	console.log('Servidor iniciado na porta 3000. Nao se esqueca de iniciar o servidor do mongo com o comando mongod');
