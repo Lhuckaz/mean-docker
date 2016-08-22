a7fad1c569b2faa132098515dd62372c6d593fef
Lucas <lucasafernandes94@gmail.com>
2016-08-22 17:36:05 -0300

    Update and rename server.js to app.js

--- app/server.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-var app = require('./config/express');
-
-http.createServer(app)
-.listen(3000, function() {
-	console.log('Servidor iniciado na porta 3000. Nao se esqueca de iniciar o servidor do mongo com o comando mongod');
-});
